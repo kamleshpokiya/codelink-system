829aded829135612203c681b09d9351baba9e4b7
krishna <sk@codelinkinfotech.com>
2022-07-08 11:16:42 +0530

    further implement in holiday module

--- admin/application/controllers/Holiday.php
+++ admin/application/controllers/Holiday.php
@@ -3,34 +3,54 @@ ob_start();
 ob_clean();
 class Holiday extends loadFile
 {
-    public $db;
-    public function __construct()
-    {
-        session_start();
-        $this->db = $this->model('Model');
-    }
-    public function holidays(){
-        $tbl = 'holiday_tbl';
+	public $db;
+	public function __construct()
+	{
+		session_start();
+		$this->db = $this->model('Model');
+	}
+	public function holidays()
+	{
+		$tbl = 'holiday_tbl';
 		$select = '';
 		$option = '';
 		$where = '';
 		$records = $this->db->select_data($select, $tbl, $option, $where);
 		$this->view("holidays", array("title" => "this is holiday list", "data" => $records));
-    }
-    public function add_holidays(){
-        $this->view("add_holiday", array("title" => "this is holiday page"));
-
-    }
-    public function edit_holidays($id){
-        $tbl = 'holiday_tbl';
+	}
+	public function add_holidays()
+	{
+		$this->view("add_holiday", array("title" => "this is holiday page"));
+		if (isset($_POST['submit'])) {
+			$date = $this->db->escape_string($_POST['date']);
+			$title = $this->db->escape_string($_POST['title']);
+			$description = $this->db->escape_string($_POST['description']);
+			$file_name = $_FILES['image']['name'];
+			$file_size = $_FILES['image']['size'];
+			$file_tmp = $_FILES['image']['tmp_name'];
+			$file_type = $_FILES['image']['type'];
+			if (isset($file_name)) {
+				move_uploaded_file($file_tmp, "assets/images/holiday_img/" . $file_name);
+			}
+			$data = array("date" => $date, "title" => $title, "description" => $description, "image" => $file_name);
+			$ins = $this->db->insert_data($data, 'holiday_tbl');
+			if ($ins) {
+				header("location:" . base_url . "Holiday/holidays");
+			}
+		}
+	}
+	public function edit_holidays($id)
+	{
+		$tbl = 'holiday_tbl';
 		$select = '';
 		$option = '';
-		$where = 'id='.$id;
+		$where = 'id=' . $id;
 		$records = $this->db->select_data($select, $tbl, $option, $where);
-        $this->view("add_holiday", array("title" => "this is holiday page", "data" => $records));
-    }
-    public function delete_holidays(){
-        if (isset($_REQUEST['did'])) {
+		$this->view("add_holiday", array("title" => "this is holiday page", "data" => $records));
+	}
+	public function delete_holidays()
+	{
+		if (isset($_REQUEST['did'])) {
 			$id = $_REQUEST['did'];
 			if (empty($id)) {
 				echo "no record found to delete";
@@ -46,5 +66,5 @@ class Holiday extends loadFile
 				}
 			}
 		}
-    }
+	}
 }
--- admin/application/views/holidays.php
+++ admin/application/views/holidays.php
@@ -4,8 +4,13 @@
             <div class="col-12 grid-margin">
                 <div class="card">
                     <div class="card-body">
+                        <a href="<?php echo base_url; ?>Holiday/add_holidays"><button class="btn btn-warning btn-fw" style="position:absolute; top:30%; right:40px;" id="adduser" class="card-title">Add Holiday</button></a>
                         <h4 class="card-title">Holidays</h4>
-                        <a href="<?php echo base_url; ?>Holiday/add_holidays"><button class="btn btn-warning btn-fw" style="position:absolute; top:5%; right:40px;" id="adduser">Add Holiday</button></a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        
                         <div class="table-responsive">
                             <table class="table">
                                 <thead>
@@ -30,7 +35,7 @@
                                                 <td><?php echo $key->date; ?></td>
                                                 <td><?php echo $key->title; ?></td>
                                                 <td><?php echo $key->description; ?></td>
-                                                <td><img class="img-xs rounded-circle" style="height:35px; width:35px;" src="<?php echo site_url; ?>images/holiday_img/<?php echo $key->image; ?>" alt=""></td>
+                                                <td><img class="img-xs rounded-circle" style="height:100px; width:200px; border-radius: 5% !important;" src="<?php echo site_url; ?>images/holiday_img/<?php echo $key->image; ?>" alt="<?php echo $key->image; ?>"></td>
                                                 <td>
                                                     <a href="<?php echo base_url; ?>Holiday/edit_holidays/<?php echo $key->id; ?>"><button type="button" class="btn btn-info btn-icon-text">
                                                             <i class="mdi mdi-view-grid"></i> Edit </button></a>
